35f8bf3b4351719b0812c78d28b0f797a5440a89
Yuvraj <jindalyuvraj4@gmail.com>
2024-11-27 14:07:57 -0500

    Updated Auth Callback

--- client/src/components/AuthCallback.js
+++ client/src/components/AuthCallback.js
@@ -9,20 +9,13 @@ function AuthCallback({ setIsAuthenticated }) {
     const params = new URLSearchParams(location.search);
     const token = params.get('token');
     const userId = params.get('userId');
-    
+
     if (token && userId) {
-      // Store token and userId in localStorage
       localStorage.setItem('token', token);
       localStorage.setItem('userId', userId);
-      
-      // Set authentication state to true
       setIsAuthenticated(true);
-      
-      // Get the previous location (if any) or default to '/recipes'
-      const previousLocation = location.state?.from || '/recipes';
-      navigate(previousLocation);
+      navigate('/recipes');
     } else {
-      // Redirect to login if token or userId is missing
       navigate('/login');
     }
   }, [location, navigate, setIsAuthenticated]);
